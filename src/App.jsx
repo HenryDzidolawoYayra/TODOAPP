4e1529c98195efe85b9aa9c822505aa0d4114a98
Dzidolawo Henry Yayra <henrydzidolawo@gmail.com>
2026-07-31 20:55:32 +0000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header";
+import Todo from "./Components/Todo";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Todo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Form.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import styles from "./form.module.css";
+const Form = ({ todos, setTodos }) => {
+  const [todo, setTodo] = useState({ name: "", done: false });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo({ name: "", done: false });
+  }
+  return (
+    <form className={styles.todoform} onSubmit={handleSubmit}>
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.modernInput}
+          placeholder="Enter todo item..."
+          type="text"
+          onChange={(e) => setTodo({ name: e.target.value, done: false })}
+          value={todo.name}
+        />
+        <button className={styles.modernButton} type="submit">
+          Add
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
